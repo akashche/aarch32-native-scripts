91b2aed7dc08ebcbcb7c98531cb9aa2f9e5f5d51
akashche <akashche@redhat.com>
2016-05-04 10:23:55 +0000

    switch jdk9 to slowdebug

--- conf-jdk9.sh
+++ conf-jdk9.sh
@@ -2,11 +2,12 @@
 
 set -x
 
-DEBUG_LEVEL=release
+DEBUG_LEVEL=slowdebug
 BOOT_JDK=/home/$USER/jdk1.8.0_65/
 
 bash ./configure \
     --disable-warnings-as-errors \
+    --disable-zip-debug-info \
     --disable-sjavac \
     --with-debug-level=$DEBUG_LEVEL \
     --with-jvm-variants=core \
