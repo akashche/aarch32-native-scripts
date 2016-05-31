c5ce2c421ffa23d761029af8fb88a3a019dd3690
akashche <akashche@redhat.com>
2016-05-31 09:50:32 +0000

    jdk9 boot scripts

--- conf-jdk9-boot.sh
+++ conf-jdk9-boot.sh
@@ -3,11 +3,12 @@
 set -x
 
 DEBUG_LEVEL=slowdebug
-BOOT_JDK=/home/$USER/jdk1.7.0_60/
+BOOT_JDK=/home/$USER/jdk9/build/linux-aarch32-normal-core-fastdebug/images/jdk
 
 bash ./configure \
-    --disable-sjavac \
+    --disable-warnings-as-errors \
     --disable-zip-debug-info \
+    --disable-sjavac \
     --with-debug-level=$DEBUG_LEVEL \
     --with-jvm-variants=core \
     --with-boot-jdk=$BOOT_JDK
--- conf-jdk9-fast.sh
+++ conf-jdk9-fast.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-DEBUG_LEVEL=slowdebug
-BOOT_JDK=/home/$USER/jdk1.7.0_60/
+DEBUG_LEVEL=fastdebug
+BOOT_JDK=/home/$USER/jdk1.8.0_65/
 
 bash ./configure \
+    --disable-warnings-as-errors \
     --disable-sjavac \
-    --disable-zip-debug-info \
     --with-debug-level=$DEBUG_LEVEL \
     --with-jvm-variants=core \
     --with-boot-jdk=$BOOT_JDK
